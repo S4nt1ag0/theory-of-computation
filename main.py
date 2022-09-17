850f86d540c17a9215f614ddefced61a1f68f067
S4nt1ag0 <gustavosantiago1018@gmail.com>
2022-09-17 18:50:55 -0300

    Removendo os comentarios

--- main.py
+++ main.py
@@ -1,38 +1,10 @@
-'''
-states = input().split(' ')
-alphabet = input().split(' ')
-ribbonAlphabet = input().split(' ')
-leftSymbol = input()
-whiteSymbol = input()
-transactions = int(input())
-
-
-
-futureStatesDictionary = {}
-
-for state in states:
-    futureStatesDictionary[state] = {}
-
-for i in range(transactions):
-    stateFrom, letterRead, stateTo, letterWrite, direction = input().split(' ')
-
-    if (stateFrom, letterRead) not in futureStatesDictionary[stateFrom]:
-        futureStatesDictionary[(stateFrom, letterRead)] = []
-
-    futureStatesDictionary[(stateFrom, letterRead)].append([stateTo, letterWrite, direction])
-'''
 states = ['0', '1', '2', '3', '4']
 alphabet = ['a', 'b']
 ribbonAlphabet = ['A', 'B', '*']
 leftSymbol = '<'
 whiteSymbol = '*'
 futureStatesDictionary = {'0': {}, '1': {}, '2': {}, '3': {}, '4': {}, ('0', 'a'): [['1', 'A', 'D']], ('1', 'a'): [['1', 'a', 'D']], ('1', 'B'): [['1', 'B', 'D']], ('1', 'b'): [['2', 'B', 'E']], ('2', 'B'): [['2', 'B', 'E']], ('2', 'a'): [['2', 'a', 'E']], ('2', 'A'): [['0', 'A', 'D']], ('0', 'B'): [['3', 'B', 'D']], ('3', 'B'): [['3', 'B', 'D']], ('3', '*'): [['4', '*', 'E']]}
-'''
-initialState = input()
-finalStates = input().split(' ')
 
-words = input().split(' ')
-'''
 initialState = '0'
 finalStates = ['4']
 
