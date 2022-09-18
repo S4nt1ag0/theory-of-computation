d08cd51a642796a8693826c03b4518a89c961730
S4nt1ag0 <gustavosantiago1018@gmail.com>
2022-09-17 21:39:54 -0300

    v1 funcional

--- main.py
+++ main.py
@@ -1,56 +1,51 @@
-states = input().split(' ')
-alphabet = input().split(' ')
-ribbonAlphabet = input().split(' ')
+states = input().split()
+alphabet = input().split()
+ribbonAlphabet = input().split()
 leftSymbol = input()
 whiteSymbol = input()
 transactions = int(input())
 
-
-
 futureStatesDictionary = {}
 
-for state in states:
-    futureStatesDictionary[state] = {}
-
 for i in range(transactions):
-    stateFrom, letterRead, stateTo, letterWrite, direction = input().split(' ')
-
-    if (stateFrom, letterRead) not in futureStatesDictionary[stateFrom]:
+    stateFrom, letterRead, stateTo, letterWrite, direction = input().split()
+    if (stateFrom, letterRead) not in futureStatesDictionary:
         futureStatesDictionary[(stateFrom, letterRead)] = []
 
     futureStatesDictionary[(stateFrom, letterRead)].append([stateTo, letterWrite, direction])
 
 initialState = input()
-finalStates = input().split(' ')
+finalStates = input().split()
 
-words = input().split(' ')
+words = input().split()
 
 for word in words:
-    ribbon = list((leftSymbol+word+whiteSymbol))
+    ribbon = list((leftSymbol + word + whiteSymbol))
     path = [(initialState, 1, ribbon)]
-    flag = 0
 
     while True:
-        state, position, ribbonElements = path.pop();
+        state, position, ribbonElements = path.pop()
 
         if (state, ribbonElements[position]) in futureStatesDictionary:
             for transaction in futureStatesDictionary[(state, ribbonElements[position])]:
                 elementsOfRibbon = []
+
                 for element in ribbonElements:
                     elementsOfRibbon.append(element)
 
-                if elementsOfRibbon[position] == whiteSymbol:
-                    elementsOfRibbon.append(whiteSymbol)
+                if ribbonElements[position] == whiteSymbol:
                     ribbonElements.append(whiteSymbol)
+                    elementsOfRibbon.append(whiteSymbol)
 
                 elementsOfRibbon[position] = transaction[1]
 
                 if transaction[2] == 'E':
-                    path.append((transaction[0],position - 1, elementsOfRibbon))
+                    path.append((transaction[0], (position - 1), elementsOfRibbon))
                 elif transaction[2] == 'D':
-                    path.append((transaction[0], position + 1, elementsOfRibbon))
+                    path.append((transaction[0], (position + 1), elementsOfRibbon))
                 elif transaction[2] == 'I':
                     path.append((transaction[0], position, elementsOfRibbon))
+
         else:
             if state in finalStates:
                 print('S')
